8762ad0a36c66a1e72e2c89e8bfb30012c8df2af
Dvivi200 <diegovillarrubia2007@gmail.com>
2026-03-20 11:42:21 +0100

    Pendent d'acabar

--- src/prog2/model/LlistaAllotjaments.java
+++ src/prog2/model/LlistaAllotjaments.java
@@ -1,4 +1,57 @@
 package prog2.model;
 
-public class LlistaAllotjaments {
+import prog2.vista.ExcepcioCamping;
+
+import java.util.ArrayList;
+
+public class LlistaAllotjaments implements InLlistaAllotjaments{
+
+    private ArrayList<Allotjament> llistaAllotjaments;
+
+    LlistaAllotjaments(){
+        llistaAllotjaments = new ArrayList<>();
+    }
+
+    @Override
+    public void afegirAllotjament(Allotjament allotjament) throws ExcepcioCamping {
+        llistaAllotjaments.add(allotjament);
+    }
+
+    @Override
+    public void buidar() {
+        llistaAllotjaments.clear();
+    }
+
+    @Override
+    public String llistarAllotjaments(String estat) throws ExcepcioCamping {
+        return "";
+    }
+
+    @Override
+    public boolean containsAllotjamentOperatiu() {
+        boolean op = false;
+        int i = 0;
+
+        while(!op){
+            if(llistaAllotjaments.get(i).isOperatiu())
+                op = true;
+            else
+                i++;
+        }
+        return op;
+    }
+
+    @Override
+    public boolean contains(Allotjament allotjament) {
+        return false;
+    }
+
+    @Override
+    public Allotjament getAllotjament(String id) throws ExcepcioCamping {
+        for(Allotjament allotjament : llistaAllotjaments){
+            if(allotjament.getId().equals(id))
+                return allotjament;
+        }
+        throw new ExcepcioCamping("Allotjament no trobat");
+    }
 }
--- src/prog2/model/TascaManteniment.java
+++ src/prog2/model/TascaManteniment.java
@@ -2,6 +2,20 @@ package prog2.model;
 
 public class TascaManteniment implements InTascaManteniment {
 
+    private int num;
+    private Allotjament allotjament;
+    private String data;
+    private TipusTascaManteniment tipus;
+    private int dies;
+
+    public TascaManteniment(int num, TipusTascaManteniment tipus, Allotjament allotjament, String data, int dies){
+        this.num = num;
+        this.tipus = tipus;
+        this.allotjament = allotjament;
+        this.data = data;
+        this.dies = dies;
+    }
+
     public static enum TipusTascaManteniment {
         Reparacio,
         Neteja,
@@ -10,56 +24,60 @@ public class TascaManteniment implements InTascaManteniment {
     };
     @Override
     public int getNum() {
-        return 0;
+        return num;
     }
 
     @Override
     public TascaManteniment.TipusTascaManteniment getTipus() {
-        return null;
+        return tipus;
     }
 
     @Override
     public Allotjament getAllotjament() {
-        return null;
+        return allotjament;
     }
 
     @Override
     public String getData() {
-        return "";
+        return data;
     }
 
     @Override
     public int getDies() {
-        return 0;
+        return dies;
     }
 
     @Override
     public void setNum(int num_) {
-
+        this.num = num_;
     }
 
     @Override
     public void setTipus(TascaManteniment.TipusTascaManteniment tipus_) {
-
+        this.tipus = tipus_;
     }
 
     @Override
     public void setAllotjament(Allotjament allotjament_) {
-
+        this.allotjament = allotjament_;
     }
 
     @Override
     public void setData(String data_) {
-
+        this.data = data_;
     }
 
     @Override
     public void setDies(int dies_) {
+        this.dies = dies_;
+    }
 
+    public String toString(){
+        return "";
     }
 
     @Override
     public String getIluminacioAllotjament() {
-        return "";
+        return allotjament.getIluminacio();
     }
 }
