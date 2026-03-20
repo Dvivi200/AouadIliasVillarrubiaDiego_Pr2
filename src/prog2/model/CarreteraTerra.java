d2118a9f460a16c688a92e51d201765f8671f72f
Dvivi200 <diegovillarrubia2007@gmail.com>
2026-03-20 10:31:30 +0100

    Classe acces i subclasses acabades

--- src/prog2/model/CarreteraTerra.java
+++ src/prog2/model/CarreteraTerra.java
@@ -2,5 +2,23 @@ package prog2.model;
 
 public class CarreteraTerra extends AccesTerra {
 
+    private float amplada;
 
+    public CarreteraTerra(String nom, boolean accesibilitat, float longitud, float amplada) {
+        super(nom, accesibilitat, longitud);
+        this.amplada = amplada;
+    }
+
+    @Override
+    public boolean isAccessibilitat() {
+        return true;
+    }
+
+    public float getAmplada() {
+        return amplada;
+    }
+
+    public void setAmplada(float amplada) {
+        this.amplada = amplada;
+    }
 }
